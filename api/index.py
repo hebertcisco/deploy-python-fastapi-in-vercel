6f1dc7b516312f5d07fde2b393f7e9a11b540ebc
hebertcisco <hebertcisco@outlook.com>
2022-12-17 22:56:53 -0300

    refactor: code ident

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
+    
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
