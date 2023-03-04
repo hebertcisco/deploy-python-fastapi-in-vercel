c0388877f3eb2c84f087a77c7ce40edf8698fd0e
hebertcisco <hebertcisco@outlook.com>
2023-03-04 14:01:11 -0300

    update api/index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,1 @@
-from fastapi import FastAPI
-
-from api.dtos.ISayHelloDto import ISayHelloDto
-
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-    
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-@app.post("/hello")
-async def hello_message(dto: ISayHelloDto):
-    return {"message": f"Hello {dto.message}"}
+from src.index import app
