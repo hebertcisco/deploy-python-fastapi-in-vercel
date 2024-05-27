cafedc47402da551df2e7de4bfb56ae8d4d732ac
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-26 20:27:47 -0400

    Update DB_Connection.py

--- src/DB_Connection.py
+++ src/DB_Connection.py
@@ -1,16 +1,16 @@
-#from pymongo.mongo_client import MongoClient
-#from pymongo.server_api import ServerApi
-#
-#class DB_Connection:
-#        def __init__(self):   
-#            uri = "mongodb+srv://adminSeed:process.env.DB_PASS@productstable.5h6sox3.mongodb.net/?retryWrites=true&w=majority&appName=ProductsTable"
-#            
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+class DB_Connection:
+        def __init__(self):   
+            uri = "mongodb+srv://adminSeed:process.env.DB_PASS@productstable.5h6sox3.mongodb.net/?retryWrites=true&w=majority&appName=ProductsTable"
+            
             # Create a new client and connect to the server
             #client = MongoClient(uri, server_api=ServerApi('1'))
-#            client = MongoClient(uri)
+            client = MongoClient(uri)
             # Send a ping to confirm a successful connection
-#            try:
-#                client.admin.command('ping')
-#                print("Pinged your deployment. You successfully connected to MongoDB!")
-#            except Exception as e:
-#                print(e)
+            try:
+                client.admin.command('ping')
+                print("Pinged your deployment. You successfully connected to MongoDB!")
+            except Exception as e:
+                print(e)
