1d59ccc5fe14810de09aae28151a5fc6650ad4d4
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-29 18:43:26 -0400

    Update db.py

--- src/db.py
+++ src/db.py
@@ -7,8 +7,12 @@ def get_client():
         uri = "mongodb://"+os.environ["USER"]+":"+os.environ["DB_PASS"]+"@ac-ufuhqvj-shard-00-00.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-01.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-02.5h6sox3.mongodb.net:27017/?ssl=true&replicaSet=atlas-1vge86-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ProductsTable"
         # Create a new client and connect to the server
         client = MongoClient(uri)
-                
-        # Send a ping to confirm a successful connection      
+        return client
+
+
+
+
+# Send a ping to confirm a successful connection      
 def ping(client):
         try:
                 client.admin.command('ping')
