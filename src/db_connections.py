1ed31dac54c4df80e51f8c57408cc47628e29226
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-29 16:26:06 -0400

    Update db_connections.py

--- src/db_connections.py
+++ src/db_connections.py
@@ -4,7 +4,7 @@ import os
 class DB_Connection:
         def __init__(self):
                 
-                uri = os.environ["MONGODB_URI"]
+                uri = "mongodb://"+os.environ["USER"]":"+os.environ["DB_PASS"]+"@ac-ufuhqvj-shard-00-00.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-01.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-02.5h6sox3.mongodb.net:27017/?ssl=true&replicaSet=atlas-1vge86-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ProductsTable"
 
                 # Create a new client and connect to the server
                 self.client = MongoClient(uri)
