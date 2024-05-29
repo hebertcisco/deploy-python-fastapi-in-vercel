310db59d4ccc498263d525b1341cf9436f8e5f51
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-29 14:50:36 -0400

    Update index.py

--- src/index.py
+++ src/index.py
@@ -5,22 +5,11 @@ import logging
 from src.dtos.ISayHelloDto import ISayHelloDto
 from pymongo.mongo_client import MongoClient
 
-class DB_Connection:
-        def __init__(self):
-                uri = "mongodb://adminSeed:process.env.DB_PASS@ac-ufuhqvj-shard-00-00.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-01.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-02.5h6sox3.mongodb.net:27017/?ssl=true&replicaSet=atlas-1vge86-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ProductsTable"
 
-                # Create a new client and connect to the server
-                client = MongoClient(uri)
                 
-                # Send a ping to confirm a successful connection
-                try:
-                    client.admin.command('ping')
-                    print("Pinged your deployment. You successfully connected to MongoDB!")
-                except Exception as e:
-                    print(e)
 
-connection = DB_Connection()
-
+                
+uri = "mongodb://adminSeed:process.env.DB_PASS@ac-ufuhqvj-shard-00-00.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-01.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-02.5h6sox3.mongodb.net:27017/?ssl=true&replicaSet=atlas-1vge86-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ProductsTable"
 app = FastAPI()
 
 origins = [
@@ -42,7 +31,17 @@ app.add_middleware(
 
 
 @app.get("/")
-async def root():
+async def root():   
+    client = MongoClient(uri)
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        
+    except Exception as e:
+        print(e)  
+                        
+               
+    
     return {"message": "Hello World"}
 
 
