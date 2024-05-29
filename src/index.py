3cbfbf3ad049b2362f8743eb27f3633c09dd1fd4
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-29 18:58:48 -0400

    Update index.py

--- src/index.py
+++ src/index.py
@@ -58,7 +58,7 @@ async def ping():
 @app.get("/tasks")
 async def get_tasks():
     client = get_client()
-    tasks_collection = client.get_collection('sample_mflix.users')
+    tasks_collection = client['sample_mflix.users']
     tasks = list(tasks_collection.find({}).limit(25))
     for task in tasks:
         task["_id"] = str(task["_id"])
