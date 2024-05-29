24151bccfaf9faace9978a239ade3a64148b6884
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-29 15:16:49 -0400

    Update DB_Connection.py

--- src/DB_Connection.py
+++ src/DB_Connection.py
@@ -5,12 +5,15 @@ class DB_Connection:
                 uri = "mongodb://adminSeed:process.env.DB_PASS@ac-ufuhqvj-shard-00-00.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-01.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-02.5h6sox3.mongodb.net:27017/?ssl=true&replicaSet=atlas-1vge86-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ProductsTable"
 
                 # Create a new client and connect to the server
-                client = MongoClient(uri)
+                self.client = MongoClient(uri)
                 
                 # Send a ping to confirm a successful connection
+                
+        def ping(self):
                 try:
-                    client.admin.command('ping')
-                    print("Pinged your deployment. You successfully connected to MongoDB!")
+                    self.client.admin.command('ping')
+                    return {"message":"Successful Ping"}
                 except Exception as e:
-                    print(e)
+                    return {"message":"Failed"}
+                
 
