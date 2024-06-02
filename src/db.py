d082d3b939b4dd35eeaa4d10cac07971e0a3a00d
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-06-02 11:59:23 -0400

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,23 +1,16 @@
 from pymongo import MongoClient
 import os
-client = None
 
-def get_client():
-        global client
-        uri = "mongodb://"+os.environ["USER"]+":"+os.environ["DB_PASS"]+"@ac-ufuhqvj-shard-00-00.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-01.5h6sox3.mongodb.net:27017,ac-ufuhqvj-shard-00-02.5h6sox3.mongodb.net:27017/?ssl=true&replicaSet=atlas-1vge86-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ProductsTable"
-        # Create a new client and connect to the server
-        client = MongoClient(uri)["ProductsTable"]
-        return client
+db = None
 
+def get_mongo_client():
+    global db
+    if db is None:
+        uri = os.environ.get('MONGODB_ATLAS_URI')
+        db = MongoClient(uri, appname="devrel.content.vercel")['TaskDB']
+    return db
 
-
-#double comment
-# Send a ping to confirm a successful connection      
-def client_ping(client):
-        try:
-                client.admin.command('ping')
-                return {"message":"Successful Ping"}
-        except Exception as e:
-                return {"message":"Failed"}
-                
+def get_collection(collection_name):
+    if db is None:
+        get_mongo_client()
 
